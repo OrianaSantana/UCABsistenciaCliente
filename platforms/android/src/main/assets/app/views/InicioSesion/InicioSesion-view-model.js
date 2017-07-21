71e8108e71070d6f87ebfc5e32382ed3439225cf
Oriana <santanaoriana11@gmail.com>
2017-07-20 20:08:38 -0400

    POST de localizacion
    Se envia la data del magnetometro recolectada, hacia el servicio y se
    recibe la respuesta de en donde se esta

--- platforms/android/src/main/assets/app/views/InicioSesion/InicioSesion-view-model.js
+++ platforms/android/src/main/assets/app/views/InicioSesion/InicioSesion-view-model.js
@@ -2,15 +2,69 @@ var Observable = require("data/observable").Observable;
 var fetchModule = require("fetch");
 var frameModule = require("ui/frame");
 var config = require("../../shared/config");
+var dialogs = require("ui/dialogs");
+//var LocalNotifications = require("nativescript-local-notifications");
 
 function createViewModel(info) {
     info = info || {};
     var viewModel = new Observable({
         usuario: info.usuario || ""
     });
+ 
+    //llamado a notificacion prueba  
+    /*doAddOnMessageReceivedCallback(); 
+    function doAddOnMessageReceivedCallback() {
+        console.log("do add");
+        console.log("do add " + LocalNotifications.addOnMessageReceivedCallback);
+        LocalNotifications.addOnMessageReceivedCallback(
+                function(notificationData) {
+                    console.log("message recived callback ");
+                    dialogs.alert({
+                        title: "Notification received",
+                        message: "ID: " + "notificationData.id" +
+                        "\nTitle: " + "notificationData.title" +
+                        "\nBody: " + "notificationData.body",
+                        okButtonText: "Excellent!"
+                    });
+                }
+            )
+        }   
+    
+    schedule();
+    
+    function schedule(){
+            console.log("function schedule");
+            LocalNotifications.schedule([{
+            id: 1,
+            title: 'The title',
+            body: 'Recurs every minute until cancelled',
+            ticker: 'The ticker',
+            //badge: 1,
+            //groupedMessages:["The first", "Second", "Keep going", "one more..", "OK Stop"], //android only
+            //groupSummary:"Summary of the grouped messages above", //android only
+            //ongoing: true, // makes the notification ongoing (Android only)
+            //smallIcon: 'res://heart',
+            //interval: 'minute',
+           // sound: require("application").ios ? "customsound-ios.wav" : "customsound-android", // can be also `null`, "default"
+            at: new Date(new Date().getTime() + (10 * 1000)) // 10 seconds from now                
+            }]).then(
+                function() {
+                dialogs.alert({
+                    title: "scheduled",
+                    message: 'notificacion ',
+                    okButtonText: "OK!"
+                });
+                },
+                function(error) {
+                console.log("doScheduleEveryMinute error: " + error);
+                }
+            );        
+    };*/
+
 
     viewModel.consultarProfesorID = function()
     {   console.log("consultar profesor por correo " + viewModel.get("usuario"));
+        //return fetchModule.fetch("http://192.168.1.6:8080/WSUcabsistencia/api.ucabsistencia.com/profesores/profesor_correo?profesor-correo=" + viewModel.get("usuario") ) 
         return fetchModule.fetch( config.apiUrl + "profesores/profesor_correo?profesor-correo=" + viewModel.get("usuario") ) 
         .then(handleErrors)
         .then(function(response) {
@@ -21,11 +75,34 @@ function createViewModel(info) {
         });        
         
     };
+
+    viewModel.validarSesion = function(_id)
+    {
+        console.log("modificar id del profesor " + _id);
+        //return fetch("http://192.168.1.6:8080/WSUcabsistencia/api.ucabsistencia.com/profesor/estatus/conexion"
+        return fetchModule.fetch( config.apiUrl + "profesor/estatus/conexion", {  //colocar uri
+        method: "PUT",
+        body: JSON.stringify({
+            Usu_foto:'on', 
+            Usu_id: _id,
+            }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(handleErrors)
+    .then(function(response) {
+        console.log("response " + response);
+        return response.json();   
+    })        
+
+    };
+    
     viewModel.consultarHorario = function(_id)
     {
         console.log("BUSCANDO HORARIOOOO ");
+        //return fetchModule.fetch("http://192.168.1.6:8080/WSUcabsistencia/api.ucabsistencia.com/profesores/horarios?profesor-id=" + _id ) 
         return fetchModule.fetch( config.apiUrl + "profesores/horarios?profesor-id=" + _id ) 
-        //return fetchModule.fetch("http://192.168.1.7:8080/WSUcabsistencia/api.ucabsistencia.com/profesores-correo/horarios?profesor-correo=" + viewModel.get("usuario") )         
         .then(handleErrors)
         .then(function(response) {
             console.log("BUSCANDO HORARIOOOO primer function then " );
@@ -49,4 +126,3 @@ function handleErrors(response) {
 }
 
 exports.createViewModel = createViewModel;
-
--- platforms/android/src/main/assets/app/views/InicioSesion/InicioSesion.js
+++ platforms/android/src/main/assets/app/views/InicioSesion/InicioSesion.js
@@ -7,6 +7,7 @@ var ls_profesor = require('local-storage');
 var ls_horario = require('local-storage');
 var dialogsModule = require("ui/dialogs");
 var ls_validator = require('local-storage');
+//var LocalNotifications = require("nativescript-local-notifications");
 
 var user;
 
@@ -16,11 +17,16 @@ exports.loaded = function(args) {
     {
         console.log("UCABsistencia Inicio");
         var page = args.object;
-        user = createViewModel();  
-        page.bindingContext = user;
-        ls_validator('validar', 0);
-    }
+        //LocalNotifications.requestPermission().then((granted) => {
+            //if(granted) {
+                    user = createViewModel();  
+                    page.bindingContext = user;
+                    ls_validator('validar', 0);        
+            //}
+        //})
+     }
 };
+
 exports.IniciarSesion = function() {
    //console.log(ls.get('foo'));
 
@@ -33,36 +39,56 @@ exports.IniciarSesion = function() {
             return Promise.reject();
         })
         .then(function(respuesta1) {          
-           console.log("RESPUESTA profesor " + respuesta1.pro_id); 
+           console.log("RESPUESTA profesor " + respuesta1.pro_id);
+           console.log("Status conexion " + respuesta1.pro_foto); 
            ls_profesor('profesor', respuesta1.pro_primer_nombre);
            ls_profesor('id', respuesta1.pro_id);
            ls_correo('correo',respuesta1.pro_correo_nombre_usuario);
            
            var id =  respuesta1.pro_id;
-
-            user.consultarHorario(respuesta1.pro_id)
-                    .catch(function(error) {  
-                        console.log("CAtch de la funtion consultar horario");        
-                        dialogsModule.alert({
-                            message: "No se pudo encontrar el horario del profesor que busca",
-                            okButtonText: "OK"
-                        });
-                        console.log("No condiguio horario iniciosesionjs");
-                        return Promise.reject();
-                    })
-                    .then(function(respuesta1) {            
-                    /*console.log("RESPUESTA  "+ respuesta1[0].hor_catedra + " " + respuesta1[1].hor_catedra + " " + respuesta1[2].hor_catedra + " " + respuesta1[3].hor_catedra + " " + respuesta1[4].hor_catedra +
-                    " "+ respuesta1[5].hor_catedra);*/
-                   ls_horario('horario_profesor', respuesta1);
-                    var navigationOptions= {
-                        moduleName:'views/Menu/home/home',
-                        context:{ 
-                                    horario: respuesta1, 
-                                    profesor: id 
-                        }
-                    }                                           
-                        frameModule.topmost().navigate(navigationOptions);                                   
-                    });           
+           if (respuesta1.pro_foto == "off"){
+               user.validarSesion(respuesta1.pro_id)
+                    .catch(function(error) {
+                            console.log(error);
+                            dialogsModule.alert({
+                                message: "No se pudo actualizar su status",
+                                okButtonText: "OK"
+                            });
+                            console.log("No PUDO ACTUALIZAR STATUS");
+                            return Promise.reject();
+                        })
+                        .then(function() {
+                            console.log("status ACTUALIZADO");                                    
+                            user.consultarHorario(respuesta1.pro_id)
+                                .catch(function(error) {  
+                                    console.log("CAtch de la funtion consultar horario");        
+                                    dialogsModule.alert({
+                                        message: "No se pudo encontrar el horario del profesor que busca",
+                                        okButtonText: "OK"
+                                    });
+                                    console.log("No condiguio horario iniciosesionjs");
+                                    return Promise.reject();
+                                })
+                                .then(function(respuesta1) {            
+                                /*console.log("RESPUESTA  "+ respuesta1[0].hor_catedra + " " + respuesta1[1].hor_catedra + " " + respuesta1[2].hor_catedra + " " + respuesta1[3].hor_catedra + " " + respuesta1[4].hor_catedra +
+                                " "+ respuesta1[5].hor_catedra);*/
+                            ls_horario('horario_profesor', respuesta1);
+                                var navigationOptions= {
+                                    moduleName:'views/Menu/home/home',
+                                    context:{ 
+                                                horario: respuesta1, 
+                                                profesor: id 
+                                    }
+                                }                                           
+                                    frameModule.topmost().navigate(navigationOptions);                                   
+                                });
+                    });  
+           }else if(respuesta1.pro_foto == "on"){
+                    dialogsModule.alert({
+                    message: "Usted tiene una sesión abierta",
+                    okButtonText: "OK"
+                  });
+           }         
         });
     /*user.consultarHorario()
         .catch(function(error) {
